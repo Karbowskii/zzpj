5933803b5de755f41e4ffc61d18d7cd25e31fff1
Karbowskii <arturkarbowskitest@gmail.com>
2020-04-10 18:35:02 +0200

    Project configuration (Docker-compose)

---
+++ backend/src/main/java/pl/zzpj/esportbetting/EsportbettingApplication.java
@@ -0,0 +1,13 @@
+package pl.zzpj.esportbetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportbettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportbettingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/zzpj/esportbetting/EsportbettingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zzpj.esportbetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportbettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
