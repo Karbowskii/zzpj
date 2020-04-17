e241fac74740c3975c14cfc3d049cbe49d044648
piotr3290 <piotr3290@wp.pl>
2020-04-17 20:10:56 +0200

    router and bootstrap configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+const router = new VueRouter({routes});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router,
+}).$mount('#app');
