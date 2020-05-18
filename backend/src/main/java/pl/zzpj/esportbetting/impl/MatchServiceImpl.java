6c2ffb4bc16d0a2514768e66379152e36dd68e9e
Michacy45 <pes.mich@gmail.com>
2020-05-18 13:29:34 +0200

    usuniecie matchResponse, dodanie adnotacji service

--- backend/src/main/java/pl/zzpj/esportbetting/impl/MatchServiceImpl.java
+++ backend/src/main/java/pl/zzpj/esportbetting/impl/MatchServiceImpl.java
@@ -1,11 +1,13 @@
 package pl.zzpj.esportbetting.impl;
 
+import org.springframework.stereotype.Service;
 import pl.zzpj.esportbetting.model.Match;
 import pl.zzpj.esportbetting.repos.MatchRepository;
 import pl.zzpj.esportbetting.services.MatchService;
 
 import java.util.List;
 
+@Service("matchService")
 public class MatchServiceImpl implements MatchService {
     private final MatchRepository matchRepository;
 
--- backend/src/main/java/pl/zzpj/esportbetting/rest/MatchRestController.java
+++ backend/src/main/java/pl/zzpj/esportbetting/rest/MatchRestController.java
@@ -5,13 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.zzpj.esportbetting.impl.MatchToMatchResponseConverter;
 import pl.zzpj.esportbetting.model.Match;
-import pl.zzpj.esportbetting.response.MatchResponse;
 import pl.zzpj.esportbetting.services.MatchService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/matches")
@@ -24,10 +21,7 @@ public class MatchRestController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<MatchResponse>> findAll(){
-        return ResponseEntity.ok(matchService.findAll()
-                .stream()
-                .map(MatchToMatchResponseConverter::convert)
-                .collect(Collectors.toList()));
+    public ResponseEntity<List<Match>> findAll(){
+        return ResponseEntity.ok(matchService.findAll());
     }
 }
