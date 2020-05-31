f0652a56431ff069926bf4cc333244a6e254ae2d
Michacy45 <pes.mich@gmail.com>
2020-05-31 23:06:36 +0200

    Jeszcze nie dziala wyswietlanko

---
+++ frontend/src/Core/HttpRequest.js
@@ -0,0 +1,42 @@
+export default class HttpRequest{
+
+    constructor(baseUrl, authorizationStorage){
+        this.baseUrl = baseUrl;
+        this.authorizationStorage = authorizationStorage;
+    }
+
+    get(url){
+        return this.execute(url, 'GET');
+    }
+
+    post(url, data){
+        return this.execute(url, 'POST', data);
+    }
+
+    put(url, data){
+        return this.execute(url, 'PUT', data);
+    }
+
+    delete(url){
+        return this.execute(url, 'DELETE');
+    }
+
+    execute(url, method, data){
+        return fetch(`${this.baseUrl}/${url}`, {
+            method: method,
+            headers: new Headers({'Content-Type': 'application/json'}),
+            credentials: 'same-origin',
+            cache: 'no-cache',
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error("HTTP status " + response.status);
+                }
+                return response.json();
+            })
+            .catch(e => {
+                return {errors: [e]}
+            })
+    }
+}
